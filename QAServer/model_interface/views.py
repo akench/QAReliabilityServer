3c71fc351d5c617e87ae6549bd5d3e3b2ac0c7f5
SChakravorti21 <shoumyoarchit@gmail.com>
2018-06-26 23:23:13 -0400

    Working on link clicking

--- QAServer/model_interface/views.py
+++ QAServer/model_interface/views.py
@@ -1,68 +1,95 @@
-import json, random, string
+import json
+import random
+import string
 
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from caching.models import Report
 
 hrefs_collected = set()
+
+
 def collect_data(request):
-	if request.method != 'POST':
-		return HttpResponseBadRequest('''This endpoint only accepts POST requests. \
+    if request.method != 'POST':
+        return HttpResponseBadRequest('''This endpoint only accepts POST requests. \
 			\rTry again with a JSON payload.''')
-	
-	body = json.loads(request.body)
-	href = body['href']
+    body = json.loads(request.body)
+    href = body['href']
 
-	if href not in hrefs_collected:
-		# Create the filename from the first ten words of the question
-		filename = 'collection/'
-		filename += '_'.join(body['brainly_data']['question'].split(maxsplit=10)[:10])
-		# filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
-		filename += '.json'
-		with open(filename, 'w') as f:
-			json.dump(body, f)
-		hrefs_collected.add(href)
+    if href not in hrefs_collected and has_valid_answers(body):
+        # Create the filename from the first ten words of the question
+        filename = 'collection/'
+        filename += '_'.join(body['brainly_data']
+                             ['question'].split(maxsplit=10)[:10])
+        # filename = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))
+        filename += '.json'
+        with open(filename, 'w') as f:
+            json.dump(body, f)
+        hrefs_collected.add(href)
 
-	results = get_inference('', '')
-	return JsonResponse(results)
+        print('SAVED FILE: ' + filename)
+    else:
+        print('NOT SAVING')
 
-# Create your views here.
+    results = get_inference('', '')
+    return JsonResponse(results)
+
 def generate_report(request):
-	if request.method != 'POST':
-		return HttpResponseBadRequest('''This endpoint only accepts POST requests. \
+    if request.method != 'POST':
+        return HttpResponseBadRequest('''This endpoint only accepts POST requests. \
 			\rTry again with a JSON payload.''')
 
-	# Parse JSON body
-	body = json.loads(request.body)
-	print(body)
-	try:
-		site = body['site']
-		url = body['url']
-		question = body['question']
-		answer = body['answer']
-		cache = body['cache']
-	except KeyError as error:
-		return HttpResponseBadRequest('''Incomplete request, try again with all of the following parameters: 	\
+    # Parse JSON body
+    body = json.loads(request.body)
+    print(body)
+    try:
+        # site = body['site']
+        # url = body['url']
+        question = body['question']
+        answer = body['answer']
+        # cache = body['cache']
+    except KeyError:
+        return HttpResponseBadRequest('''Incomplete request, \
+                try again with all of the following parameters: 	\
 			\r1) site (string) : the website from which the QA originates 										\
 			\r2) url (string) : the raw url referring to the question	 										\
 			\r3) question (string) : the question being asked 													\
 			\r4) answer (string) : the answer to the question 													\
-			\r5) cache (boolean) : whether or not the report for this url should be cached''')					
+			\r5) cache (boolean) : whether or not the report for this url should be cached''')
 
-	# Do something to get results
-	results = get_inference(question, answer)
-	return JsonResponse(results)
+    # Do something to get results
+    results = get_inference(question, answer)
+    return JsonResponse(results)
+
 
 def get_inference(question, answer):
-	# I don't remember what the specific statistics were, so they'll
-	# be modified as necessary later.
-	ret_data = {
-		'error': False,
-		'statistics': {
-			'reliability': 50,
-			'ambiguity': 25,
-			'brevity': 80,
-			'thoroughness': 0
-		}
-	}
-	return ret_data
+    # I don't remember what the specific statistics were, so they'll
+    # be modified as necessary later.
+    ret_data = {
+        'error': False,
+        'statistics': {
+            'reliability': 50,
+            'ambiguity': 25,
+            'brevity': 80,
+            'thoroughness': 0
+        }
+    }
+    return ret_data
+
+def has_valid_answers(request_body):
+    """
+    Checks if there are any answers that are not
+    just one/few-word answers.
+    """
+
+    answers = request_body['brainly_data']['all_answers']
+
+    for answer in answers:
+        answer = answer['text'].strip()
+        print(answer)
+        if answer.count(" ") > 2:
+            return True
+
+    # Return false to indicate that the entire question is
+    # useless since there is no useful data
+    return False
--- automation/chrome_collection.py
+++ automation/chrome_collection.py
@@ -2,7 +2,24 @@ import time
 import selenium
 from selenium import webdriver
 
-chrome = None
+LINK_SELECTOR = "#search-results > div.sg-layout__content > \
+    div.sg-content-box.search-results.stream-container.sg-layout__box > \
+    div.sg-content-box__content.sg-content-box__content--spaced-bottom-large.search-results__items.js-search-items > \
+    div > article > div.sg-content-box__content > a > div > div > div"
+QUERIES = [
+    "history"
+]
+
+def visit_all(chrome, site):
+    base_url = "https://brainly.com/app/ask?entry=top&q={}".format(site)
+    chrome.get(base_url)
+    links = chrome.find_elements_by_css_selector(LINK_SELECTOR)
+
+    for link in links:
+        link.click()
+        time.sleep(2)
+        chrome.execute_script("window.history.go(-1)")
+
 def main():
     """
     Main function, creates the Chrome instance and visits the necessary websites
@@ -17,18 +34,15 @@ def main():
 
     # Create a Chrome instance and open up a website
     chrome = webdriver.Chrome(chrome_options=options)
-    chrome.get('https://brainly.com/question/10546818')
 
-    # Wait for an adequate amount of time for the data to get collected
-    time.sleep(2)
+    for query in QUERIES:
+        visit_all(chrome, query)
 
-    # Quit the browser
-    chrome.quit()
-
+chrome = None
 if __name__ == "__main__":
     try:
         main()
-    except selenium.common.exceptions.WebDriverException as exception:
+    except selenium.common.exceptions.WebDriverException as error:
         # In the event that an error occurs, we still want to quit
         # Chrome, otherwise a chromdriver instance will keep running
         if chrome:
