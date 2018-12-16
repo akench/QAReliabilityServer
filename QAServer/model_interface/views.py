37def87c094770bc90fa4e1ce60afbc422921c33
Shoumyo <shoumyoarchit@gmail.com>
2018-12-16 01:23:21 -0500

    Ready to deploy server?

--- QAServer/model_interface/views.py
+++ QAServer/model_interface/views.py
@@ -54,11 +54,11 @@ def get_features_df(answer):
 
     protocol_list = ['num_words', 'num_characters', 'num_misspelled', 'bin_start_interrogative', 'bin_end_qmark',
         'num_interrogative', 'bin_start_small', 'avg_word_sentence', 'num_sentences', 'bin_url',
-        'readability_score', 'num_punctuations', 'bin_taboo'] # 'grammar_check'
+        'readability_score', 'num_punctuations', 'bin_taboo', 'grammar_check']
     formatanswers_func_list = ['get_total_words', 'get_total_number_of_characters', 'number_of_misspelled_words',
         'check_interrogative_start', 'check_question_mark_end', 'number_of_interrogative_words',
         'check_small_letter_start', 'average_words_per_sentence', 'get_number_of_sentences',
-        'check_if_url_present', 'get_readability_score', 'number_of_punctuation_marks', 'check_for_profanity'] #'grammar_check']
+        'check_if_url_present', 'get_readability_score', 'number_of_punctuation_marks', 'check_for_profanity', 'grammar_check']
 
     for index, method_name in enumerate(formatanswers_func_list):
         runnable_method = getattr(formatter, method_name)
